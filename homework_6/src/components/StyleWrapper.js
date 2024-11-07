4084120fdac1a9a015e891ec67cb7fb8b7a8d0a9
Ocloa <93639857+Ocloa@users.noreply.github.com>
2024-11-07 10:19:07 +0700

    Delete homework_6 directory

--- homework_6/src/components/StyleWrapper.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const StyleWrapper = (Component, styles) => (props) => (
-    <div style={styles}>
-        <Component {...props} />
-    </div>
-);
-
-export default StyleWrapper;
