0a816ffde256654cbcbf33afe7274a8cf456cf00
Ocloa <93639857+Ocloa@users.noreply.github.com>
2024-11-07 12:36:39 +0700

    Delete homework_9 directory

--- homework_9/src/store/IBlenderStore.ts
+++
@@ -1,7 +0,0 @@
-export interface IBlenderStore {
-    selectedNumbers: number[];
-    operation: string;
-    toggleNumber: (num: number) => void;
-    setOperation: (op: string) => void;
-    result: number;
-}
